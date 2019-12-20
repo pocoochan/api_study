3a34f39de6af3dedb334c71f08c034001dd483a0
Saya <sayamatsui327@gmal.com>
2019-12-20 18:08:29 +0900

    天気予報のAPIがjsで呼び出せた！

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ https://api.openweathermap.org/data/2.5/weather?APPID=4b5774e9f3d2a07b84f0f2f88e
 
 */
 
-fetch('https://holidays-jp.github.io/api/v1/date.json')
+fetch('https://api.openweathermap.org/data/2.5/weather?APPID=4b5774e9f3d2a07b84f0f2f88e486224&q=London')
   .then(response => response.text())
   .then(text => {
     console.log(text);
