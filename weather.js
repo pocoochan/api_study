0afd94ff87ef3f6c2be22e62f4c91a03a8262e91
Saya <sayamatsui327@gmal.com>
2019-12-20 18:26:30 +0900

    清書

--- weather.js
+++ weather.js
@@ -1,14 +1,6 @@
-
-/*
-https://api.openweathermap.org/data/2.5/weather?APPID=4b5774e9f3d2a07b84f0f2f88e486224&q=London
-
-これでAPIが動くよ！
-
-*/
-
 fetch('https://api.openweathermap.org/data/2.5/weather?APPID=4b5774e9f3d2a07b84f0f2f88e486224&q=London')
-  .then(response => response.text())
-  .then(text => {
-    var data = JSON.parse(text);
-    console.log(data.weather[0].description);
-  });
+.then(response => response.text())
+.then(text => {
+  var data = JSON.parse(text);
+  console.log(data.weather[0].description);
+});
